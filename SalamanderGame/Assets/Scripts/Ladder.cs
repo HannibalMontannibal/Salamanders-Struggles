d202d7d85fb64613dee11e549c97e347f0424149
JuliannaPfister <33308827+JuliannaPfister@users.noreply.github.com>
2018-04-12 09:11:31 +1000

    Player's Basic Mov
    I created walk, jum and Sprint Mechanichs.

---
+++ SalamanderGame/Assets/Scripts/Ladder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    public float speed = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
+       
+        {
+
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0,speed);
+        }
+
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+
+        {
+
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+
+        else
+        {
+           other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1.20f);
+           
+        }
+    }
+}
